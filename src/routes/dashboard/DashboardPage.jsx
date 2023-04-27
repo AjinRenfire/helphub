3f91cde5225502efeb00263628a45c413cd90bd7
AjinRenfire <ajinra4@outlook.com>
2023-04-27 15:01:19 +0530

    Update DashboardPage.jsx

--- src/routes/dashboard/DashboardPage.jsx
+++ src/routes/dashboard/DashboardPage.jsx
@@ -7,8 +7,8 @@ import './dashboard.css'
 export default function DashboardPage(){
 
     const fakeValues = {
-        Name:"Homelander",
-        Hobbies:"killing,More killing",
+        Name:"Ramu",
+        Hobbies:"Reading,More reading",
         Email:"homelander@vought.com",
         Location:"Chennai",
         Phone:"+91 78430 23785"
@@ -33,7 +33,7 @@ export default function DashboardPage(){
                 <div className="card">
                     <h3>About me</h3>
                     <p>
-                        Homelander is the leader of The Seven, the strongest Supe in the world, and the archenemy of Billy Butcher, the leader of The Boys. With the face of a movie star and the power of a god, Homelander is considered the greatest superhero alive. Not only can he fly, but he possesses super hearing, super strength and super durability far beyond most supes, and can see through anything except for zinc with his X-ray vision, as well as being able to burn and cut through things by shooting a red-colored beam of heat energy from out of his eyes.
+                       Ramu Sonu are twins. They are the sons of Tinku and Mintu. They have two two twin-sisters. They are Dholu Bolu
                     </p>
                 </div>
             </section>
